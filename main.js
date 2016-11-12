6a97281b78b952a5d00e4292cf315520bdd04c57
Vikas Magar <vikasmagar512@gmail.com>
2016-11-13 01:35:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	// code by www.voidcanvas.com
+	var textDiv = $("#textDiv");
+	var sentence = textDiv.html().split("");
+	var vcTimeline = new TimelineMax({
+		repeat : 1,
+		repeatDelay : 0.4,
+		yoyo : true
+	});
+
+	textDiv.html("");
+
+	$.each(sentence, function(index, val) {
+		if (val == " ") {
+			val = "&nbsp;";
+		}
+		var letter = $("<span/>", {
+			id : "txt" + index
+		}).html(val).appendTo(textDiv);
+		vcTimeline.to(letter, 1, {
+			rotationY : 360,
+			rotationX : 360,
+			textShadow : "0px 0px 0px black",
+			color : "none",
+			autoAlpha : 1,
+			ease : Back.easeOut,
+			transformOrigin : "50% 50% -20"
+		}, index * 0.05);
+	});
+	
+
+}); 
